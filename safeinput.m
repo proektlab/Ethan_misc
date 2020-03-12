35675c86535d30772067b1f16f3260c76cc3341d
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-03-12 12:43:21 -0400

    Add validation message in  and fix bugs

--- safeinput.m
+++ safeinput.m
@@ -1,18 +1,35 @@
-function x = safeinput(prompt, validator)
-% input that reprompts instead of errors if you make a typo
-% also reprompts if input does't pass validator function, if used
+function x = safeinput(prompt, validator, val_msg)
+% input that reprompts instead of errors if you make a typo.
+% also reprompts if input does't pass validator function, if used.
+% if input does not pass validation, issues a warning including the
+% optional val_msg, which should be formatted as a sentence.
 
-if nargin < 2
+if nargin < 2 || isempty(validator)
     validator = @(~) true;
 end
 
+if nargin < 3
+    val_msg = '';
+end
+
 while true
+    raw_input = input(prompt, 's');
     try
-        x = input(prompt);
+        % use eval to get around weird reprompting behavior of `input`
+        if isempty(raw_input)
+            x = [];
+        else
+            x = eval(raw_input);
+        end
+
         if validator(x)
             return;
+        else
+            warning(['Invalid input - try again. ', val_msg]);
         end
     catch
+        warning('Invalid syntax - try again.');
     end
 end
+
 end
