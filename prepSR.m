ea49a80f58f63b5ed3c02f19113b771e8a4b225b
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-03-21 13:00:47 -0400

    Add snippits dir and gitignore

--- prepSR.m
+++ prepSR.m
@@ -4,11 +4,13 @@ global raw_dir;
 global processed_lfp_dir;
 global script_dir;
 global results_dir;
+global snippits_dir;
 
 project_dir = fullfile(synology_dir, 'brenna', 'States_rats');
 raw_dir = fullfile(project_dir, 'RawData');
 processed_lfp_dir = fullfile(project_dir, 'meanSubtracted_fullTrace');
 results_dir = fullfile(project_dir, 'forEthan', 'spec-state-trans');
 script_dir = fullfile(fileparts(mfilename('fullpath')), '..', 'spec-state-trans');
+snippits_dir = fullfile(project_dir, 'Snippits');
 
 addpath(script_dir);
