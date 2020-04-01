c95b8db28c47db6f67b6cc0d603d85fda2dfd785
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-04-01 14:46:36 -0400

    Correct exp_filter for even width and width of 1

--- exp_filter.m
+++ exp_filter.m
@@ -6,8 +6,20 @@ function [b, a] = exp_filter(width_2t, decay_2t)
 % manner using filtfilt. At the point where this 2-tailed impulse response reaches
 % decay_2t times the peak, the width will be width_2t samples.
 
-amp = -2 * log(decay_2t) / width_2t;
-b = amp;
-a = [1, -exp(-amp)];
+assert(width_2t >= 1, 'Width must be positive');
+
+if width_2t == 1 % make a no-op
+    b = 1;
+    a = 1;
+else
+    if mod(width_2t, 2) == 0
+        % we want it to be odd, just add 1
+        width_2t = width_2t + 1;
+    end
+
+    amp = -2 * log(decay_2t) / (width_2t - 1);
+    b = amp;
+    a = [1, -exp(-amp)];
+end
 
 end
