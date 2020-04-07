4e3cc53140d13b413d7d96f25a22de2efedc5cf2
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-04-07 09:51:36 -0400

    Use SpectralAnalysis repo

--- prepSR.m
+++ prepSR.m
@@ -6,11 +6,16 @@ global script_dir;
 global results_dir;
 global snippits_dir;
 
+this_dir = fileparts(mfilename('fullpath'));
+
 project_dir = fullfile(synology_dir, 'brenna', 'States_rats');
 raw_dir = fullfile(project_dir, 'RawData');
 processed_lfp_dir = fullfile(project_dir, 'meanSubtracted_fullTrace');
 results_dir = fullfile(project_dir, 'forEthan', 'spec-state-trans');
-script_dir = fullfile(fileparts(mfilename('fullpath')), '..', 'spec-state-trans');
+script_dir = fullfile(this_dir, '..', 'spec-state-trans');
 snippits_dir = fullfile(project_dir, 'Snippits');
 
 addpath(script_dir);
+
+% other dependencies
+addpath(fullfile(this_dir, '..', 'SpectralAnalysis'));
