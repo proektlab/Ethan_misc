3d71c81c262c1d8fe893ee738ab5bc9b01a113a9
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-04-28 11:21:25 -0400

    Just use jet and allow customizing ylims for class_plot

--- class_plot.m
+++ class_plot.m
@@ -1,47 +1,54 @@
-function h = class_plot(ax, xaxis, class_vec)
+function h = class_plot(varargin)
 % class_plot: colored area plot showing classifications over time
 % class_vec should be a numeric vector of class assignments; classes are plotted in sorted order.
-% If there are more than 7 classes, patches are distinguished by edge color.
 %
-% Syntaxes:
-%     h = class_plot([ax]=gca, [xaxis]=1:length(class_vec), class_vec)
+% Syntax:
+%     h = class_plot([ax]=gca, [xaxis]=1:length(class_vec), [ylims]=[0,1], class_vec)
 %
 % Returns h, a vector of patch objects, one per class.
 
-assert(nargin >= 1, 'Must at least provide class_vec input');
+assert(~isempty(varargin), 'Must at least provide class_vec input');
 
-if nargin < 3
-    if ~isa(ax, 'matlab.graphics.axis.Axes')
-        if nargin < 2
-            class_vec = ax;
-            xaxis = 1:numel(class_vec);
-        else
-            class_vec = xaxis;
-            xaxis = ax;
-        end
-        ax = gca;
-    else
-        class_vec = xaxis;
-        xaxis = 1:length(class_vec);
-    end
+% last argument is always class_vec
+class_vec = varargin{end};
+assert(isvector(class_vec) && isnumeric(class_vec) && isreal(class_vec), 'class_vec must be a real vector');
+n_pts = length(class_vec);
+
+% check optional arguments
+optargs = varargin(1:end-1);
+
+if ~isempty(optargs) && isa(optargs{1}, 'matlab.graphics.axis.Axes')
+    ax = optargs{1};
+    optargs = optargs(2:end);
+else
+    ax = gca;
 end
 
-% validation
-assert(isa(ax, 'matlab.graphics.axis.Axes'), 'Invalid axes provided');
-assert(isvector(xaxis) && isreal(xaxis) && isnumeric(xaxis) && all(diff(xaxis) > 0), 'Invalid xaxis');
-assert(isvector(class_vec) && isreal(class_vec) && isnumeric(class_vec), 'class_vec must be a real vector');
-assert(length(class_vec) == length(xaxis), 'Length of class_vec doesn''t match length of xaxis');
+if ~isempty(optargs) && isvector(optargs{1}) && length(optargs{1}) == n_pts
+    xaxis = optargs{1}(:).';
+    assert(isnumeric(xaxis) && isreal(xaxis) && all(diff(xaxis) > 0), 'Invalid xaxis');
+    optargs = optargs(2:end);
+else
+    xaxis = 1:n_pts;
+end
 
-xaxis = xaxis(:).';
+% quit early if it looks like x axis was the wrong length
+if ~isempty(optargs) && length(optargs{1}) > 2 && length(optargs{1}) ~= n_pts
+    error('Invalid argument - probably x axis is the wrong length');
+end
+
+if ~isempty(optargs) && numel(optargs{1}) == 2
+    ylims = optargs{1};
+    optargs = optargs(2:end);
+else
+    ylims = [0, 1];
+end
+
+assert(isempty(optargs), 'Too many or invalid arguments (see help for syntax)');
 
 classes = unique(class_vec);
 n_classes = length(classes);
-
-if n_classes <= 7
-    colors = lines(n_classes);
-else
-    colors = jet(n_classes);
-end
+colors = jet(n_classes);
 
 halfdt = mean(diff(xaxis)) / 2;
 
@@ -60,17 +67,14 @@ for kC = 1:n_classes
     class_end_times = min(xaxis(end), class_end_times + halfdt);
 
     X = [class_start_times; class_end_times; class_end_times; class_start_times];
-    Y = repmat([1; 1; 0; 0], 1, size(X, 2));
+    Y = repmat([ylims(1); ylims(1); ylims(2); ylims(2)], 1, size(X, 2));
 
-    h(kC) = patch(ax, X, Y, colors(kC, :), 'LineStyle', 'none');
+    h(kC) = patch(ax, X, Y, colors(kC, :), 'LineStyle', 'none', 'DisplayName', num2str(class));
 end
 
-ylim(ax, [0, 1]);
-yticks(ax, []);
-xlim(ax, [xaxis(1), xaxis(end)]);
+axis tight;
 
-legend(ax, arrayfun(@num2str, classes, 'uni', false), ...
-    'Location', 'southoutside', 'NumColumns', 8);
+legend(ax, 'Location', 'southoutside', 'NumColumns', 8);
 
 end
 
