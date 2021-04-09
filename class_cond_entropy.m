d6a54cb0248133ef9ff2acbb565c237e4518c27d
Ethan Blackwood <ethanbblackwood@gmail.com>
2021-04-09 18:27:57 -0400

    Extend mutual info to work on 2 disjoint sets of vectors

--- class_cond_entropy.m
+++ class_cond_entropy.m
@@ -1,34 +1,37 @@
-function cent = class_cond_entropy(X)
-% Computes the conditional/noise entropy of each class
-% assignment vector given each other vector.
-%
-% Input: each column of X should be a vector of m class assignments.
-% Vectors need not have the same number of classses.
-%
-% Ouptut: m x m matrix of conditional entropy with zero diagonal.
-% Element i,j is the conditional entropy of
-% assignment j given assignmnent i.
+function cent = class_cond_entropy(X, Y)
+% Computes the conditional/noise entropy of each class assignment vector
+% in Y given each vector in X. X and Y must have the same size in dimension 1.
+% 
+% If Y is omitted, each column of X is compared with each other column and the
+% diagonal is 0 (by definition).
+% 
+% Output: If X is n x p and Y is n x q, output is a p x q matrix.
 
-[n, m] = size(X);
-cent = zeros(m);
+[n, p] = size(X);
 
-all_classes = unique(X(:));
-
-for i = 1:m
-    i_classes = unique(X(:, i));
-
-    js = [1:i-1, i+1:m];
-
-    cent_i = zeros(1, m-1);
-
-    for k = 1:length(i_classes)
-        kX = X(X(:, i) == i_classes(k), js);
-        pK = size(kX, 1) / n;
-
-        cent_i = cent_i + pK * class_entropy(kX, all_classes);
+if nargin == 1
+    cent = zeros(p);
+    for i = 1:p
+        not_i = [1:i-1, i+1:p];
+        cent(i, not_i) = class_cond_entropy(X(:, i), X(:, not_i));
     end
-
-    cent(i, js) = cent_i;
+    return;
 end
 
+% now we definitely have Y
+[m, q] = size(Y);
+assert(n == m, 'X and Y must have the same length (dimension 1)');
+cent = zeros(p, q);
+
+all_y_classes = unique(Y(:));
+
+for i = 1:p
+    i_classes = unique(X(:, i));
+    for k = 1:length(i_classes)
+        kY = Y(X(:, i) == i_classes(k), :);
+        pK = size(kY, 1) / n;
+        
+        cent(i, :) = cent(i, :) + pK * class_entropy(kY, all_y_classes);
+    end
+end
 end
--- class_mut_info.m
+++ class_mut_info.m
@@ -1,14 +1,23 @@
-function [minfo, norm_minfo] = class_mut_info(X)
+function [minfo, norm_minfo] = class_mut_info(X, Y)
 % Similar to class_cond_entropy, but computes
 % mututal information rather than conditional entropy.
+%
+% If one argument is provided, gives mutual info between all pairs of columns.
+% If both X and Y are provided, gives mutual info between columns of X and columns of Y.
 %
 % Second output is the normalized mutual information
 % (https://course.ccs.neu.edu/cs6140sp15/7_locality_cluster/Assignment-6/NMI.pdf)
 
-ent = class_entropy(X); % (1 x m vector)
-cent = class_cond_entropy(X); % (m x m matrix)
-
-minfo = ent - cent;
-norm_minfo = 2 * minfo ./ (ent + ent');
+ent_x = class_entropy(X); % (1 x p vector)
+if nargin == 2
+    ent_y = class_entropy(Y); % (1 x q vector)
+    cent = class_cond_entropy(X, Y); % (p x q matrix)
+else
+    ent_y = ent_x;
+    cent = class_cond_entropy(X);
+end
+    
+minfo = ent_y - cent;
+norm_minfo = 2 * minfo ./ (ent_x' + ent_y);
     
 end
