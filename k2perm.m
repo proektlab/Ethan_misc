6c5a29de0e3526c80a8aa55165d100a90f44db1a
Ethan Blackwood <ethanbblackwood@gmail.com>
2021-06-03 11:36:33 -0400

    Optimize k2perm

--- k2perm.m
+++ k2perm.m
@@ -3,14 +3,21 @@ function perm = k2perm(n, k)
 % gen_kth_perm(n, k) is different from gen_kth_perm(n, j) iff k ~= j
 % and gen_kth_perm(n, 1) is 1:n.
 
-assert(k >= 1 && k <= factorial(n), 'Invalid k');
-
-perm = zeros(1, n);
-perm(1) = floor((k-1)/factorial(n-1)) + 1;
-
-if n > 1
-    rest = setdiff(1:n, perm(1));
-    perm(2:end) = rest(k2perm(n-1, mod(k-1, factorial(n-1))+1));
+persistent factorials;
+if length(factorials) < n
+    factorials = [factorials, factorial(length(factorials)+1:n)];
 end
+
+assert(k >= 1 && k <= factorials(n), 'Invalid k');
+
+if n == 1
+    perm = 1;
+    return;
+end
+    
+perm = zeros(1, n);
+perm(1) = floor((k-1)/factorials(n-1)) + 1;
+rest = [1:perm(1)-1, perm(1)+1:n];
+perm(2:end) = rest(k2perm(n-1, mod(k-1, factorials(n-1))+1));
 
 end
