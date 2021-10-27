24ee9d2ff83bcdd1540e9e52d677f996d6d45e17
Ethan Blackwood <ethanbblackwood@gmail.com>
2021-10-27 10:29:28 -0400

    Use newer version of EEGLAB

--- startup.m
+++ startup.m
@@ -1,7 +1,7 @@
 % paths
 synology_dir = get_synology_dir;
 
-addpath(fullfile(synology_dir, 'brenna', 'Software', 'eeglab2019_1'));
+addpath(fullfile(synology_dir, 'brenna', 'Software', 'eeglab2021.1'));
 
 % disable axis toolbar
 set(groot, 'defaultFigureCreateFcn', @(fig,~) addToolbarExplorationButtons(fig));
