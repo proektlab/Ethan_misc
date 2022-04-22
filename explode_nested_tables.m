eab53e64a9c6186a7553d88a7621fdecf58e81f8
Ethan Blackwood <ethanbblackwood@gmail.com>
2022-04-22 16:13:38 -0400

    add explode_nested_tables

---
+++ explode_nested_tables.m
@@ -0,0 +1,14 @@
+function new_table = explode_nested_tables(outer_table)
+% vertically concatenate all nested tables in the 'data' variable into one,
+% and repeat other columns to match.
+
+nondata_cols = setdiff(outer_table.Properties.VariableNames, 'data');
+new_table = vertcat(outer_table.data{:});  % stack inner tables
+rep_nums = cellfun(@height, outer_table.data);
+
+for kCol = 1:length(nondata_cols)
+    col_name = nondata_cols{kCol};
+    new_table.(col_name) = repelem(outer_table.(col_name), rep_nums);
+end
+
+end
