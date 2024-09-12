26ef859cea1a20376f7ed03abd6238aa92a4c56b
Ethan Blackwood <ethanbblackwood@gmail.com>
2024-09-12 11:48:41 -0400

    Remove eeglab from startup

--- startup.m
+++ startup.m
@@ -1,8 +1,6 @@
 % paths
 synology_dir = get_synology_dir;
 
-addpath(fullfile(synology_dir, 'brenna', 'Software', 'eeglab2021.1'));
-
 % disable axis toolbar
 set(groot, 'defaultFigureCreateFcn', @(fig,~) addToolbarExplorationButtons(fig));
 
