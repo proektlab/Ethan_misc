c7812c8324a6f368008e0604b0d628960ccdc968
Ethan Blackwood <ethanbblackwood@gmail.com>
2024-11-05 13:42:30 -0500

    Don't mess with axis toolbar

--- startup.m
+++ startup.m
@@ -1,9 +1,6 @@
 % paths
 synology_dir = get_synology_dir;
 
-% disable axis toolbar
-set(groot, 'defaultFigureCreateFcn', @(fig,~) addToolbarExplorationButtons(fig));
-
 global good_green
 good_green = [0.39608, 0.67843, 0];
 
