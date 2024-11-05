e24be14095d4e6ff801284e19061ed91cf50a469
Ethan Blackwood <ethanbblackwood@gmail.com>
2024-11-05 13:45:10 -0500

    Get rid of eeglab, add bigdata

--- startup.m
+++ startup.m
@@ -1,13 +1,11 @@
 % paths
-synology_dir = get_synology_dir;
-
-addpath(fullfile(synology_dir, 'brenna', 'Software', 'eeglab2021.1'));
-
-% disable axis toolbar
-set(groot, 'defaultFigureCreateFcn', @(fig,~) addToolbarExplorationButtons(fig));
+[synology_dir, bigdata_dir] = get_synology_dir;
 
 global good_green
 good_green = [0.39608, 0.67843, 0];
 
 % can always use rng('default') if I want to get consistent results
 rng('shuffle');
+
+% necessary on treachery for some reason
+matlabpath(pathdef);
